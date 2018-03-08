527da02c8cecf7da4da6b3d56341695f3c3f329f
xjc@xjc.com <xjc>
2018-03-08 09:39:17 +0800

    init

--- wordtohtml/wordtohtml/add_image_to_word.py
+++ wordtohtml/wordtohtml/add_image_to_word.py
@@ -9,12 +9,14 @@ def add_image_to_word(images=[]):
 
     try:
         word=win32.gencache.EnsureDispatch("Word.Application")
+        word.DisplayAlerts  = False
         print word,11111111111
         doc=word.Documents.Add()
         for img in images:
             word.Selection.InlineShapes.AddPicture(FileName=img,LinkToFile= False,SaveWithDocument=True)
         doc.SaveAs("c:\\tmp.doc")
         doc.Close(True)
+        #doc.Quit()
         word.Application.Quit()
         return "c:\\tmp.doc"
     except Exception,e:
